90a53ef841ccab0834d68cba8d78dbb0a4a32134
noonena <euniceleow46@gmail.com>
2024-07-05 17:01:03 +0700

    lastversion

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-<?php echo "<h1>My respository</h1>">
-<?php echo "<h1>My name is Eunice</h1>">
+<?php echo "<h1>My respository</h1>"?>
+<?php echo "<h1>My name is Eunice</h1>"?>
+<?php echo "<br>"?>
+<?php echo "Mai mee tung TT"?>
--- menu.php
+++ menu.php
@@ -1,7 +1,8 @@
 <?php echo "Main"?>
 <?php echo "<h1>Mem</h1>"?>
 <?php echo "<br>"?>
-<?php echo "<br>"?>
 <?php echo "<h1>Memory</h1>"?>
 <?php echo "<br>"?>
 <?php echo "<h1>Hais</h1>"?>
+<?php echo "<br>"?>
+<?php echo "<h1>Dai ja</h1>"?>
